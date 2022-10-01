94688afc9837eefb031540a7a34e57ff77b979a4
Erick Willian <87780768+EriickW@users.noreply.github.com>
2022-10-01 17:01:37 -0300

    .

--- TypeScript/dist/js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Precisa identificar o tipo da variavel
-const x = 10;
-function soma(a, b) {
-    return a + b;
-}
-console.log(soma(1, 4));
--- TypeScript/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Precisa identificar o tipo da variavel
-const x: number = 10;
-
-function soma(a:number, b: number){
-    return a + b
-}
-
-console.log( soma(1,4)) 
